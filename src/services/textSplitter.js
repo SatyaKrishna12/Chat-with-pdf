464a201051d6b2d05904514f2428e362b955e9f7
SatyaKrishna12 <satyakrishna883@gmail.com>
2025-07-16 10:43:35 +0530

    Added more chunks

--- src/services/textSplitter.js
+++ src/services/textSplitter.js
@@ -2,6 +2,8 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
 const getTextChunks = async (text) => {
     const textSplitter = new RecursiveCharacterTextSplitter({
+        // chunkSize: 1500,
+        // chunkOverlap: 300,
         chunkSize: 10000,
         chunkOverlap: 1000,
     });
